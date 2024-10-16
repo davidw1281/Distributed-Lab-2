3babd1f2a610c15535a0a62664d792fb8ae11227
David <davidwusu128@icloud.com>
2024-10-16 18:45:34 +0100

    Client

--- secretstrings/server/server.go
+++ secretstrings/server/server.go
@@ -1,23 +1,45 @@
-
 package main
+
 import (
-//	"errors"
-//	"flag"
-//	"fmt"
-//	"net"
-	"time"
+	//	"errors"
+	"flag"
+	//	"fmt"
 	"math/rand"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
-//	"net/rpc"
+	"net"
+	"net/rpc"
+	"time"
+
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
 /** Super-Secret `reversing a string' method we can't allow clients to see. **/
 func ReverseString(s string, i int) string {
-    time.Sleep(time.Duration(rand.Intn(i))* time.Second)
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	time.Sleep(time.Duration(rand.Intn(i)) * time.Second)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
+type SecretStringOperations struct{}
+
+func (s *SecretStringOperations) Reverse(request stubs.Request, response *stubs.Response) (err error) {
+	response.Message = ReverseString(request.Message, 10)
+	return
+}
+
+func (s *SecretStringOperations) FastReverse(request stubs.Request, response *stubs.Response) (err error) {
+	response.Message = ReverseString(request.Message, 2)
+	return
+}
+
+func main() {
+	pAddr := flag.String("port", "8030", "Port to listen on")
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+	rpc.Register(&SecretStringOperations{})
+	listener, _ := net.Listen("tcp", ":"+*pAddr)
+	defer listener.Close()
+	rpc.Accept(listener)
+}
